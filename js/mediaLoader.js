19a1446419f06a2c6a8ba900fd8baa310db8f20d
animsai <animsai@gmail.com>
2014-05-08 14:33:39 +0200

    made MediaLoader class independent of the graphic parts, and make it only do its work, meaning load the media

--- js/mediaLoader.js
+++ js/mediaLoader.js
@@ -5,35 +5,22 @@
  */
 
 (function() {
-    function MediaLoader(manifestArray, stage) {
-        this.initialize(manifestArray, stage);
+    function MediaLoader(manifestArray) {
+        this.initialize(manifestArray);
     }
    MediaLoader.prototype = new createjs.EventDispatcher();
         MediaLoader.prototype.mediaQueue =  null;
-        MediaLoader.prototype.stage =  null;
-        MediaLoader.prototype.bar =  null;
-        MediaLoader.prototype.loadingBarText =  null;
         MediaLoader.prototype.mediaProxy = null;
         MediaLoader.prototype.allManifestConcat = null;
         MediaLoader.prototype.EventDispatcher_initialize = MediaLoader.prototype.initialize;
-        MediaLoader.prototype.initialize  =  function(manifestArray, stage) {
+        MediaLoader.prototype.initialize  =  function(manifestArray) {
             MediaLoader.prototype.EventDispatcher_initialize();
-            this.stage = stage;
             
             this.allManifestConcat = [];
             //concat all manifests to load all media at once
             for(var i=0; i<manifestArray.length; i++) {
                 this.allManifestConcat = this.allManifestConcat.concat(manifestArray[i]);
             }
-
-//            this.loadingBarText = new Moulin.LoadingBarText("Loading...", "1.2em Verdana", "black", 300, "center", 50);
-//            this.stage.addChild(this.loadingBarText);
-            var background = new createjs.Bitmap(introImg.src);
-            this.stage.addChild(background);
-
-            //creating a loading bar from our class and passing some arguments
-            this.bar = new Moulin.LoadingBar(500, 90, 5, "#72AF2C", "#8CCF3F");
-            this.stage.addChild(this.bar);
 
              //manage loading queue
             this.mediaQueue = new createjs.LoadQueue(false);
@@ -48,27 +35,11 @@
             return this;
         };
         MediaLoader.prototype.handleProgress =  function() {
-            this.bar.loadingBar.scaleX = this.mediaQueue.progress * this.bar.width;
-            progresPrecentage = Math.round(this.mediaQueue.progress * 100);
-//            this.loadingBarText.setText(progresPrecentage + "% Loaded");
+            this.dispatchEvent("assetsLoadingProgress");
         };
         MediaLoader.prototype.handleComplete =  function() {
-            this.addNextButton();
             this.dispatchEvent("assetsComplete");
         };
-        MediaLoader.prototype.addNextButton  =  function() {
-            var nextBtn = Utils.generateBitmapItem(interLevel_fileManifest[2].src, 430, 350, 1, true);
-            this.stage.removeChild(this.bar);
-            
-            this.mediaProxy = createjs.proxy(this.handleClick, this);
-            nextBtn.addEventListener("click", this.mediaProxy);
-            this.stage.addChild(nextBtn);
-        };
-        MediaLoader.prototype.handleClick = function() {
-            this.stage.removeAllEventListeners("click");
-            nav = new Moulin.Navigation(nav_fileManifest, this.stage);
-        }
-//    };
    
     Moulin.MediaLoader = MediaLoader;
 }());
--- js/moulin.js
+++ js/moulin.js
@@ -11,33 +11,59 @@
     }
     Moulin.prototype = {
         stage: null,
+        moulinProxy: null,
+        bar:null,
+        assets:null,
         init: function() {
+            this.initStage();
+            
+           this.loadScoresFromLocalStorage();
+
+            this.loadCoreAssets();
+        },
+        initStage:function() {
             var canvas = document.getElementById("gameCanvas");
             this.stage = new createjs.Stage(canvas);
             createjs.Touch.enable(this.stage);
-            var proxy = new createjs.proxy(this.handleTick, this);
-            createjs.Ticker.addEventListener("tick", proxy);
+            this.moulinProxy = new createjs.proxy(this.handleTick, this);
+            createjs.Ticker.addEventListener("tick", this.moulinProxy);
             this.onResize();
+            
+            var background = new createjs.Bitmap(introImg.src);
+            this.stage.addChild(background);
+        },
+        loadCoreAssets:function() {
+            this.assets = new Moulin.MediaLoader(allFiles, this.stage);
+            this.moulinProxy = new createjs.proxy(this.handleLoadProgress, this);
+            this.assets.addEventListener("assetsLoadingProgress", this.moulinProxy);
+            
+            this.moulinProxy = new createjs.proxy(this.addStartButton, this);
+            this.assets.addEventListener("assetsComplete", this.moulinProxy);
+            
+            //creating a loading bar from our class and passing some arguments
+            this.bar = new Moulin.LoadingBar(500, 90, 5, "#72AF2C", "#8CCF3F");
+            this.stage.addChild(this.bar);
+        },
+        addStartButton: function() {
+            var nextBtn = Utils.generateBitmapItem(interLevel_fileManifest[2].src, 430, 350, 1, true);
+            this.stage.removeChild(this.bar);
 
-            //Load scores from local storage
+            this.moulinProxy = createjs.proxy(this.handleStartClick, this);
+            nextBtn.addEventListener("click", this.moulinProxy);
+
+            this.stage.addChild(nextBtn);
+        },
+        handleStartClick: function() {
+            this.stage.removeAllEventListeners("click");
+            nav = new Moulin.Navigation(nav_fileManifest, this.stage);
+        },
+        handleLoadProgress:function() {
+            this.bar.loadingBar.scaleX = this.assets.mediaQueue.progress * this.bar.width;
+        },
+        loadScoresFromLocalStorage:function() {
             if (Utils.supportsLocalStorage() && localStorage["moulin.scores"] !== undefined) {
                 userScore = JSON.parse(localStorage["moulin.scores"]);
             }
-
-            var assets = new Moulin.MediaLoader(allFiles, this.stage);
-            assets.addEventListener("assetsComplete", function() {console.log("fini terminé");});
-//            assets.on("assetsComplete", function() {console.log("fini terminé");});
-//           var assets = new Moulin.AssetManager();
-//           assets.manageInitialLoad(allFiles, this.stage);
-//           assets.on(assets.ASSETS_COMPLETE, this.addStartButton);
-        },
-         addStartButton : function() {
-            var nextBtn = Utils.generateBitmapItem(interLevel_fileManifest[2].src, 430, 350, 1, true);
-//            this.stage.removeChild(this.bar);
-            
-//            this.mediaProxy = createjs.proxy(this.handleClick, this);
-//            nextBtn.addEventListener("click", this.mediaProxy);
-            this.stage.addChild(nextBtn);
         },
         handleTick: function() {
             this.stage.update();
@@ -65,7 +91,6 @@
 
         }
     };
-
 
     window.Moulin = Moulin;
 }());
